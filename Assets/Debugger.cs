c2e6e7cdaf6fa15a714e898d0e032ad9e8413673
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2022-07-13 12:18:04 +0200

    Debugger prototype

--- Assets/Debugger.cs
+++ Assets/Debugger.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Debugger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public TMP_Text debugText;
 }
